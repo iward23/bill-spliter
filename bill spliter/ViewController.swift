d2a241c2e95bbbecf75b48c0490d32b944c44fae
Imani Ward <idward712@gmail.com>
2023-04-18 13:13:08 -0400

    Initial Commit

---
+++ bill spliter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bill spliter
+//
+//  Created by 9i on 4/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
